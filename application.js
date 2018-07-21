adb4b65ade6bcb812a8beb96b1a8ad50b523bbaf
unrealGuthrie <recursivelaughter@gmail.com>
2018-07-21 18:44:38 +0200

    regup

--- application.js
+++ application.js
@@ -2639,7 +2639,6 @@ function startGame() {
     setCSS("initial", "display", "none");
     setCSS("windows", "display", "block");
     setCSS("menuButtons", "display", "block");
-    document.body.oncontextmenu = "return (false);";
 
     initializeButtons();
 
